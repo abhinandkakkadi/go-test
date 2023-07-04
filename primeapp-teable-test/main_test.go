26cfafdf81cb1a1b66735893bffd1a9be3f9fb8f
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-07-04 10:48:13 +0530

    add additonal test cases to primeapp

--- primeapp-teable-test/main_test.go
+++ primeapp-teable-test/main_test.go
@@ -4,30 +4,32 @@ import "testing"
 
 func Test_isPrime(t *testing.T) {
 
-	primeTests := []struct{
-		name string
-		testNum int
+	primeTests := []struct {
+		name     string
+		testNum  int
 		expected bool
-		msg string
+		msg      string
 	}{
-		{"prime",7,true,"7 is a prime number!"},
+		{"prime", 7, true, "7 is a prime number!"},
+		{"prime", 9, false, "9 is not a prime number because it is divisible by 3"},
+		{"prime", 1, false, "1 is not prime, by definition!"},
 	}
 
-	for _,e := range primeTests {
+	for _, e := range primeTests {
 
-		result,msg := isPrime(e.testNum)
+		result, msg := isPrime(e.testNum)
 
 		if e.expected && !result {
-			t.Errorf("%s: expected true but got false",e.name)
+			t.Errorf("%s: expected true but got false", e.name)
 		}
 
 		if !e.expected && result {
-			t.Errorf("%s: expected false but got true",e.name)
+			t.Errorf("%s: expected false but got true", e.name)
 		}
 
 		if e.msg != msg {
-			t.Errorf("%s: expected %s but got %s",e.name,e.msg,msg)
+			t.Errorf("%s: expected %s but got %s", e.name, e.msg, msg)
 		}
 	}
-	
+
 }
